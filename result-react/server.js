140a87ee1c4b47c57aeec89db6ec04c9a008580e
cherry <qoosdd89382@gmail.com>
2023-06-04 01:45:42 +0800

    build the result app in react

--- result-react/server.js
+++ result-react/server.js
@@ -1,3 +1,4 @@
+// 此檔獨立以 node server.js 啟動
 var express = require('express'),
     async = require('async'),
     pg = require('pg'),
@@ -8,12 +9,25 @@ var express = require('express'),
     methodOverride = require('method-override'),
     app = express(),
     server = require('http').Server(app),
-    io = require('socket.io')(server);
+    io = require('socket.io')(server, {
+      /* https://stackoverflow.com/questions/67363195/how-do-i-fix-the-following-cors-error-and-polling-error */
+      cors: {
+        origin: '*'
+      }
+    });
 
-io.set('transports', ['polling']);
+// 測試 toggle flag
+const isTestToggle = true;
 
-var port = process.env.PORT || 4000;
+// 設定Socket.io的傳輸方式為polling (long polling)
+io.transports = ['polling'];
 
+// 建立Express應用程式和HTTP伺服器
+var port = isTestToggle
+    ? 2000
+    : (process.env.PORT || 4000);
+
+// 當有客戶端與Socket.io建立連接時，傳送歡迎訊息，並處理訂閱事件
 io.sockets.on('connection', function (socket) {
 
   socket.emit('message', { text : 'Welcome!' });
@@ -23,10 +37,14 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
+// 建立一個PostgreSQL資料庫連接池
 var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: isTestToggle
+    ? 'postgres://postgres:postgres@127.0.0.1:64434/postgres'
+    : 'postgres://postgres:postgres@db/postgres'
 });
 
+// 透過async.retry函式嘗試連接資料庫。如果連接失敗，每隔1秒重新嘗試連接，最多嘗試1000次
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
@@ -42,15 +60,18 @@ async.retry(
       return console.error("Giving up");
     }
     console.log("Connected to db");
+    // 當成功連接資料庫後，調用getVotes函式
     getVotes(client);
   }
 );
 
+// 從資料庫中查詢投票結果，並使用Socket.io將結果發送給所有連接的客戶端
 function getVotes(client) {
   client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
     if (err) {
       console.error("Error performing query: " + err);
     } else {
+      // 查詢沒錯誤的時候, 呼叫collectVotesFromResult
       var votes = collectVotesFromResult(result);
       io.sockets.emit("scores", JSON.stringify(votes));
     }
@@ -59,6 +80,7 @@ function getVotes(client) {
   });
 }
 
+// 透過getVotes函式執行資料庫查詢獲取的投票結果，並將結果轉換成特定格式的投票統計數
 function collectVotesFromResult(result) {
   var votes = {a: 0, b: 0};
 
@@ -69,6 +91,7 @@ function collectVotesFromResult(result) {
   return votes;
 }
 
+// 使用Express中間件設定，包括cookie-parser、body-parser、method-override和處理跨域請求的設定
 app.use(cookieParser());
 app.use(bodyParser());
 app.use(methodOverride('X-HTTP-Method-Override'));
@@ -79,12 +102,15 @@ app.use(function(req, res, next) {
   next();
 });
 
+// 使用Express靜態中間件設定提供靜態檔案的目錄
 app.use(express.static(__dirname + '/views'));
 
+// 定義根路由的處理函式，回傳index.html檔案
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+  res.sendFile(path.resolve(__dirname + '/public/index.html'));
 });
 
+// 啟動伺服器，監聽指定的埠號，並在控制台顯示啟動訊息
 server.listen(port, function () {
   var port = server.address().port;
   console.log('App running on port ' + port);
--- result-react/src/App.js
+++ result-react/src/App.js
@@ -1,22 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const socket = io(
+    'http://localhost:2000',
+    { transports: ['polling'] }
+);
+
+const StatsCtrl = () => {
+
+  const [aPercent, setAPercent] = useState(50);
+  const [bPercent, setBPercent] = useState(50);
+  const [total, setTotal] = useState(0);
+
+  const updateScores = (data) => {
+
+    const { a, b } = JSON.parse(data);
+
+    const percentages = getPercentages(a, b);
+
+    setAPercent(percentages.a);
+    setBPercent(percentages.b);
+    setTotal(a + b);
+  };
+
+  useEffect(() => {
+    socket.on('scores', updateScores);
+
+    return () => {
+      socket.off('scores', updateScores);
+    };
+  }, []);
+
+  const getPercentages = (a, b) => {
+    const result = {};
+    console.log('a',a)
+    console.log('b',b)
+
+    if (a + b > 0) {
+      result.a = Math.round((a / (a + b)) * 100);
+      result.b = 100 - result.a;
+    } else {
+      result.a = result.b = 50;
+    }
+
+    console.log('result', result)
+    return result;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="background-stats">
+<b>{Math.random()}</b>
+      <div id="background-stats-1" style={{ width: `${aPercent}%` }}></div>
+      <div id="background-stats-2" style={{ width: `${bPercent}%` }}></div>
+      <div id="content-container">
+        <div id="content-container-center">
+          <div id="choice">
+            <div className="choice cats">
+              <div className="label">Cats</div>
+              <div className="stat">{`${aPercent.toFixed(1)}%`}</div>
+            </div>
+            <div className="divider"></div>
+            <div className="choice dogs">
+              <div className="label">Dogs</div>
+              <div className="stat">{`${bPercent.toFixed(1)}%`}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="result">
+        {total === 0 && <span>No votes yet</span>}
+        {total === 1 && <span>{total} vote</span>}
+        {total >= 2 && <span>{total} votes</span>}
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="catsvsdogs">
+      <StatsCtrl />
+    </div>
+  );
+};
 
 export default App;
