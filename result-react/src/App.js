99e6872c05e7873150f3f581d075e7d975826b59
cherry <qoosdd89382@gmail.com>
2023-06-04 12:08:20 +0800

    format index.js,App.js and fix style problem

--- result-react/src/App.js
+++ result-react/src/App.js
@@ -2,86 +2,84 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
 const socket = io(
-    'http://localhost:2000',
-    { transports: ['polling'] }
+	'http://localhost:2000',
+	{ transports: ['polling'] }
 );
 
 const StatsCtrl = () => {
 
-  const [aPercent, setAPercent] = useState(50);
-  const [bPercent, setBPercent] = useState(50);
-  const [total, setTotal] = useState(0);
+	const [aPercent, setAPercent] = useState(50);
+	const [bPercent, setBPercent] = useState(50);
+	const [total, setTotal] = useState(0);
+	document.body.style.opacity = 1;
 
-  const updateScores = (data) => {
+	const updateScores = (data) => {
+		console.log('data', data)
+		const { a, b } = JSON.parse(data);
 
-    const { a, b } = JSON.parse(data);
+		const percentages = getPercentages(a, b);
 
-    const percentages = getPercentages(a, b);
+		setAPercent(percentages.a);
+		setBPercent(percentages.b);
+		setTotal(a + b);
+	};
 
-    setAPercent(percentages.a);
-    setBPercent(percentages.b);
-    setTotal(a + b);
-  };
+	useEffect(() => {
+		socket.on('scores', updateScores);
 
-  useEffect(() => {
-    socket.on('scores', updateScores);
+		return () => {
+			socket.off('scores', updateScores);
+		};
+	}, []);
 
-    return () => {
-      socket.off('scores', updateScores);
-    };
-  }, []);
+	const getPercentages = (a, b) => {
+		const result = {};
+		console.log('a', a)
+		console.log('b', b)
 
-  const getPercentages = (a, b) => {
-    const result = {};
-    console.log('a',a)
-    console.log('b',b)
+		if (a + b > 0) {
+			result.a = Math.round((a / (a + b)) * 100);
+			result.b = 100 - result.a;
+		} else {
+			result.a = result.b = 50;
+		}
 
-    if (a + b > 0) {
-      result.a = Math.round((a / (a + b)) * 100);
-      result.b = 100 - result.a;
-    } else {
-      result.a = result.b = 50;
-    }
+		console.log('result', result)
+		return result;
+	};
 
-    console.log('result', result)
-    return result;
-  };
-
-  return (
-    <div id="background-stats">
-<b>{Math.random()}</b>
-      <div id="background-stats-1" style={{ width: `${aPercent}%` }}></div>
-      <div id="background-stats-2" style={{ width: `${bPercent}%` }}></div>
-      <div id="content-container">
-        <div id="content-container-center">
-          <div id="choice">
-            <div className="choice cats">
-              <div className="label">Cats</div>
-              <div className="stat">{`${aPercent.toFixed(1)}%`}</div>
-            </div>
-            <div className="divider"></div>
-            <div className="choice dogs">
-              <div className="label">Dogs</div>
-              <div className="stat">{`${bPercent.toFixed(1)}%`}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="result">
-        {total === 0 && <span>No votes yet</span>}
-        {total === 1 && <span>{total} vote</span>}
-        {total >= 2 && <span>{total} votes</span>}
-      </div>
-    </div>
-  );
+	return (<>
+		<div id="background-stats">
+			<div id="background-stats-1" style={{ width: `${aPercent}%` }}></div>
+			<div id="background-stats-2" style={{ width: `${bPercent}%` }}></div>
+		</div>
+		<div id="content-container">
+			<div id="content-container-center">
+				<div id="choice">
+					<div className="choice cats">
+						<div className="label">Cats</div>
+						<div className="stat">{`${aPercent.toFixed(1)}%`}</div>
+					</div>
+					<div className="divider"></div>
+					<div className="choice dogs">
+						<div className="label">Dogs</div>
+						<div className="stat">{`${bPercent.toFixed(1)}%`}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="result">
+			{total === 0 && <span>No votes yet</span>}
+			{total === 1 && <span>{total} vote</span>}
+			{total >= 2 && <span>{total} votes</span>}
+		</div>
+	</>);
 };
 
 const App = () => {
-  return (
-    <div className="catsvsdogs">
-      <StatsCtrl />
-    </div>
-  );
+	return (
+		<StatsCtrl />
+	);
 };
 
 export default App;
--- result-react/src/index.js
+++ result-react/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//import './stylesheets/style.css';
+import './stylesheets/style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('body'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
